af8e8376b1b70e87dbc53d1bd8990842c5ef0aba
cherylasif <cherylsoniaasif@yahoo.co.uk>
2020-04-14 23:07:48 +0100

    TicTacToe

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Game from '../Game/index';
+
+function App() {
+  return (
+    <div>
+      <h1> TIC TAC TOE </h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
